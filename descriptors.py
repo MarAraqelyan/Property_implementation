0f67aa1e8534921eb62c0d886089189eac1c7ec4
Mariam <mariam.araqelyan2004@gmail.com>
2023-12-28 20:02:01 +0400

    The implementation of the property decorator

---
+++ descriptors.py
@@ -0,0 +1,30 @@
+class Property:
+    def __init__(self, fget=None, fset=None, fdel=None, doc=None):
+        self.fget = fget
+        self.fset = fset
+        self.fdel = fdel
+        self.doc = doc
+
+    def __set_name__(self, owner_class, property_name):
+        self.property_name = property_name
+
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
+        print(instance, "_____", self, "____", owner)
+        return self.fget(instance)
+
+    def __set__(self, instance, value):
+        if self.fset is None:
+            raise AttributeError("can't set attribute")
+        self.fset(instance, value)
+
+    def __delete__(self, instance):
+        if self.fdel is None:
+            raise AttributeError("can't delete attribute")
+        self.fdel(instance)
+
+
+
+
+
